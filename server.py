03347a38b91eeb8d130a7a4a8ae47a75e084a2bb
Thuyvi Nguyen <vivipnguyen@gmail.com>
2020-11-19 14:17:20 -0800

    added connect to sspotify to navbar

--- server.py
+++ server.py
@@ -291,8 +291,8 @@ def play_music():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    # app.debug = True
-    app.debug = False
+    app.debug = True
+    # app.debug = False
 
     # make sure templates, etc. are not cached in debug mode
     app.jinja_env.auto_reload = app.debug
